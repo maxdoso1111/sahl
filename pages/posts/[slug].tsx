335ac3588f671609202b3ef86f85501de1d6afdf
amrjabir <118849998+amrjabir@users.noreply.github.com>
2023-06-26 21:09:48 +0300

    Update [slug].tsx

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -13,6 +13,72 @@ import Tags from '../../components/tags'
 import { getAllPostsWithSlug, getPostAndMorePosts } from '../../lib/api'
 import { CMS_NAME } from '../../lib/constants'
 
-export default function Post()
-{
+export default function Post({ post, posts, preview }) {
+  const router = useRouter()
+  const morePosts = posts?.edges
+
+  if (!router.isFallback && !post?.slug) {
+    return <ErrorPage statusCode={404} />
+  }
+
+  return (
+    <Layout preview={preview}>
+      <Container>
+        <Header />
+        {router.isFallback ? (
+          <PostTitle>Loading…</PostTitle>
+        ) : (
+          <>
+            <article>
+              <Head>
+                <title>
+                  {`${post.title}`}
+                </title>
+                <meta property="og:image" content={post.featuredImage.node.sourceUrl} />
+              </Head>
+              <PostHeader
+                title={post.title}
+                coverImage={post.featuredImage}
+                date={post.date}
+                author={post.author}
+                categories={post.categories}
+              />
+              <PostBody content={post.content} />
+              <footer>
+                {post.tags.edges.length > 0 && <Tags tags={post.tags} />}
+              </footer>
+            </article>
+
+            
+          </>
+        )}
+      </Container>
+    </Layout>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async ({
+  params,
+  preview = false,
+  previewData,
+}) => {
+  const data = await getPostAndMorePosts(params?.slug, preview, previewData)
+
+  return {
+    props: {
+      preview,
+      post: data.post,
+      posts: data.posts,
+    },
+    revalidate: 10,
+  }
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const allPosts = await getAllPostsWithSlug()
+
+  return {
+    paths: allPosts.edges.map(({ node }) => `/posts/${node.slug}`) || [],
+    fallback: true,
+  }
 }
