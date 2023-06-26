09dc154b88135771481dee19f1a5e929d9994165
amrjabir <118849998+amrjabir@users.noreply.github.com>
2023-06-26 22:51:17 +0300

    Update [slug].tsx

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -43,7 +43,6 @@ export default function Post({ post, posts, preview }) {
                 author={post.author}
                 categories={post.categories}
               />
-              <PostBody content={post.content} />
               <footer>
                 {post.tags.edges.length > 0 && <Tags tags={post.tags} />}
               </footer>
