9ad1e892325fab2ffe2981d3243d6985e9f68f16
amrjabir <118849998+amrjabir@users.noreply.github.com>
2023-06-26 22:36:33 +0300

    Update [slug].tsx

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -3,6 +3,7 @@ import ErrorPage from 'next/error'
 import Head from 'next/head'
 import { GetStaticPaths, GetStaticProps } from 'next'
 import Container from '../../components/container'
+import PostBody from '../../components/post-body'
 import Header from '../../components/header'
 import PostHeader from '../../components/post-header'
 import SectionSeparator from '../../components/section-separator'
@@ -21,17 +22,39 @@ export default function Post({ post, posts, preview }) {
   }
 
   return (
- <article>
+    <Layout preview={preview}>
+      <Container>
+        <Header />
+        {router.isFallback ? (
+          <PostTitle>Loading…</PostTitle>
+        ) : (
+          <>
+            <article>
               <Head>
                 <title>
                   {`${post.title}`}
                 </title>
+                <meta property="og:image" content={post.featuredImage.node.sourceUrl} />
               </Head>
               <PostHeader
+                title={post.title}
+                coverImage={post.featuredImage}
+                date={post.date}
+                author={post.author}
+                categories={post.categories}
               />
-              
+              <PostBody content={post.content} />
+              <footer>
+                {post.tags.edges.length > 0 && <Tags tags={post.tags} />}
+              </footer>
             </article>
-         )
+
+            
+          </>
+        )}
+      </Container>
+    </Layout>
+  )
 }
 
 export const getStaticProps: GetStaticProps = async ({
