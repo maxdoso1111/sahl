e8c4e05ae688c86473d77bf7ee3ddb1a7f6926e8
amrjabir <118849998+amrjabir@users.noreply.github.com>
2023-06-26 21:55:59 +0300

    Update [slug].tsx

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -34,19 +34,10 @@ export default function Post({ post, posts, preview }) {
                 <title>
                   {`${post.title}`}
                 </title>
-                <meta property="og:image" content={post.featuredImage.node.sourceUrl} />
               </Head>
               <PostHeader
                 title={post.title}
-                coverImage={post.featuredImage}
-                date={post.date}
-                author={post.author}
-                categories={post.categories}
               />
-              <PostBody content={post.content} />
-              <footer>
-                {post.tags.edges.length > 0 && <Tags tags={post.tags} />}
-              </footer>
             </article>
 
             
