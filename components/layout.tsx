ecc019b56f749b583a75c88814be6cbdbc5f7510
amrjabir <118849998+amrjabir@users.noreply.github.com>
2023-06-26 21:14:59 +0300

    Update layout.tsx

--- components/layout.tsx
+++ components/layout.tsx
@@ -6,11 +6,6 @@ export default function Layout({ preview, children }) {
   return (
     <>
       <Meta />
-      <div className="min-h-screen">
-        <Alert preview={preview} />
-        <main>{children}</main>
-      </div>
-      <Footer />
     </>
   )
 }
